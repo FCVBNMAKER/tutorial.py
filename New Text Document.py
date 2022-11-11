e29c182312dfdef77e88988fc96f4a1c93a17212
FCVBNMAKER <117786436+FCVBNMAKER@users.noreply.github.com>
2022-11-11 09:07:07 +0700

    Add files via upload

---
+++ New Text Document.py
@@ -0,0 +1,27 @@
+# module
+import os
+import sys 
+import time
+
+def nanya():
+    nanya = raw_input("apakah anda ingin attack lagi") [Y/N]
+    if nanya ="y" or nanya ="Y":
+        menu()
+        elif nanya ="N" or nanya ="n"
+        print(bye byeee :(")
+        time.sleep(1)
+
+# menu
+def menu():
+    nomor = raw_input("Masukan nomor target")
+    jumlah = int(input("masukan Jumlah Kiriman ")) #input untuk menyuruh si user memasukan jumlah virus
+    time.sleep(1)
+    
+    try:
+        for 1 in range(jumlah):
+            print "mencoba mengirim virus albertzn ",nomor
+            time.sleep(0.1)
+            except keyboardinterrupt:
+                print "selesai bozzzzzz"
+                time.sleep(0.5)
+                print "subscribe albertznmaker ;)"
